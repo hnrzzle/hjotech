38dedd225eeb1b515f9d6ccd99eeac6a6224f92e
Henrique Oliveira <35386798+hnrzzle@users.noreply.github.com>
2023-01-09 13:13:18 -0800

    inital scaffold, start styling

---
+++ components/MainHeading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainHeading = () => {
+  return (
+    <section className="MainHeading">
+      <h1 className="Intro">Hi, I'm Henry</h1>
+    </section>
+  );
+};
+
+export default MainHeading;
